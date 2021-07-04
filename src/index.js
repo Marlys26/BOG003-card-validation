935f6e28b5ee72ff981eef46301cb887a585c349
Marlys26 <marlysrc@gmail.com>
2021-07-04 17:03:06 -0500

    estilos y lunh

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import validator from './validator.js';
 
-console.log(validator);
+//console.log(validator);
+//Funcion para mostrar mi pagina de compra
+//function mostrarValidator() {
+//let x = document.getElementById("validator");
+  //if (x.style.display === "none") {
+    //    x.style.display = "block";
+    //} else {
+      //  x.style.display = "block";
+    //}
+//}
+
+  //  function closeValidator() {
+    //let x = document.getElementById("validator");
+    //if (x.style.display === "block") {
+      //  x.style.display = "none";
+    //} else {
+      //   x.style.display = "none";
+    //}
+    //}
+document.getElementById("btn").addEventListener("click", () => {
+    validator.isValid(document.getElementById("cardNumber").value);
+    });
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,38 @@
+
 const validator = {
-  // ...
-};
+
+    isValid: (creditCardNumber) => {
+        let sumNumber = 0;
+        let totalSum = 0;
+//Convirtiendo el string en array y reversando
+        creditCardNumber = creditCardNumber.split('').reverse();
+//vamos a iterar en las posiciones para poder hacer la multiplicacion
+        for (var i = 0; i < creditCardNumber.length; i++) {
+            //buscamos las posiciones pares
+            if (i % 2 === 1) {
+                //multiplicamos el valor de la posicion par por 2
+                creditCardNumber[i] = (creditCardNumber[i] * 2);
+                //Si es mayor a 10
+                if (creditCardNumber[i] >= 10) {
+    //Si es mayor a 10 debemos convertir el numero en string y de string a array
+                     sumNumber=creditCardNumber[i].toString().split(''); 
+                       //console.log("El valor" + sumNumber);
+    //una vez convertido en array y teniendo la posicion 0 y 1 las sumamos                  
+                     creditCardNumber[i]= parseInt(sumNumber[0])+parseInt(sumNumber[1]);
+                     //console.log("Numeros sumados" + (creditCardNumber[i]));
+                }
+            }
+//Una vez sumados, tenemos una variable acumuladora que contiene los numeros restantes de nuestro input
+        totalSum = totalSum + parseInt(creditCardNumber[i]);
+            //console.log("Total suma" + totalSum);   
+        }
+        //Pasamos aplicar nuestro modulo 10
+        let validCard = totalSum % 10 == 0 ? true : false; 
+        return validCard; 
+        
+         //console.log(validCard);
+        
+       
+}};
 
 export default validator;
